86acf6816e0cfa3f31afde16cf2b0a142cf1a41b
Tom Chandler <tchandler48@gmail.com>
2021-04-05 15:31:39 -0500

    Update

---
+++ prototyp.h
@@ -0,0 +1,363 @@
+/* ***************************************************
+* c2z : Prototyp.h :                                 *
+*                                                    *
+* Copyright TCCS (c) 2015 - 2021                     *
+**************************************************** */
+
+/* ----- function prototypes ----- */
+
+/*            c2z.c                */
+
+/*		c2z_atoi.c		*/
+	void c2_atoi(void);
+
+/*		c2z_break.c		*/
+	void c2_break(void);
+
+/*		c2z_case.c		*/
+	void c2_case(void);
+	void c2_pass2_case(void);
+
+/* 	       c2z_char.c		*/
+	void c2_falph(void);
+	void c2_falph_1(void);
+	void c2_falph_2(void);
+	void c2_falph_3(void);
+	void c2_falph_4(void);
+	void c2_falph_5(void);
+	void c2_falph_6(void);
+	void c2_falph_9(void);
+
+/*		c2z_const.c		*/
+	void c2_const(void);
+
+/*		c2z_ctime.c		*/
+	void c2_ctime(void);
+
+/* 	       c2z_debug.c		*/
+	void c2_debug(void);
+	void c2_print_files(void);
+
+/*		c2z_decr.c		*/
+	void c2_minus(void);
+
+/*		c2z_define.c 		*/
+	void c2_define(void);
+
+/* 	       c2z_double.c		*/
+	void c2_double(void);
+	void c2_unsigned(void);
+
+/*		c2z_enum.c		*/
+	void c2_enum_scan(void);
+
+/*		c2z_exit.c		*/
+	void c2_exit(void);
+
+/*		c2z_fclose.c		*/
+	void c2_close(void);
+
+/*		c2z_feof.c		*/
+	void c2_scan_feof(void);
+
+/*		c2z_fgets.c		*/
+	void c2_fgets(void);
+	void c2_scan_sc4(void);
+
+/*		c2z_file.c		*/
+	void c2_file(void);
+
+/* 	       c2z_files.c		*/
+	void c2_puts(void);
+	void c2_gets(void);
+
+/*		c2z_fopen.c		*/
+	void c2_open(void);
+	void c2_scan_sc3(void);
+
+/* 	       c2z_for.c		*/
+	void c2_for(void);
+
+/*		c2z_fputs.c		*/
+	void c2_fputs(void);
+	void c2_scan_fputs(void);
+
+/*		c2z_free.c		*/
+	void c2_free(void);
+
+/* 	       c2z_function.c	*/
+	void c2_func_call(void);	
+	void c2_func_end(void);
+	void c2_func_sub(void);
+	void c2_scan_func(void);
+	void c2_scan_sub(void);
+       void scan_func_name_1(void);
+
+/*		c2z_goto_c		*/
+	void c2_goto(void);
+	void c2_goto_label(void);
+
+/* 	       c2z_if.c		*/
+	void c2_if(void);
+	void c2_switch(void);
+	void c2_case_end(void);
+	void c2_case_default(void);
+	void if_case_1(void);
+	void if_case_2(void);
+	void if_case_3(void);
+	void if_case_4(void);
+	void if_case_6(void);
+	void if_case_7(void);
+	void if_case_8(void);
+	void if_case_9(void);
+	void if_case_10(void);
+	void if_case_11(void);
+	void if_case_12(void);
+	void if_case_13(void);
+	void if_case_15(void);
+	void if_case_22(void);
+	void if_case_23(void);
+	void if_case_24(void);
+	void if_case_30(void);
+
+/*		c2z_incr.c		*/
+	void c2_plus(void);
+
+/* 	       c2z_int.c		*/
+	void c2_int(void);
+	void c2_int_1(void);
+	void c2_int_2(void);
+	void c2_int_3(void);
+	void c2_int_5(void);
+	void c2_int_6(void);
+	void c2_int_punch(void);
+	void c2_int_punch_1(void);
+	void c2_int_punch_2(void);
+
+/*		c2z_islanum.c		*/
+	void c2_isalnum(void);
+
+/*		c2z_isalpha.c		*/
+	void c2_isalpha(void);
+
+/*		c2z_isdigit.c		*/
+	void c2_isdigit(void);
+
+/*		c2z_isspace.c		*/
+	void c2_isspace(void);	
+
+/*		c2z_isupper.c		*/
+	void c2_isupper(void);
+
+/*		c2z_localtime.c	*/
+	void c2_localtime(void);
+
+
+/*		c2z_malloc.c		*/
+	void c2_scan_malloc(void);
+
+
+/* 	       c2z_math.c		*/
+	void c2_math(void);
+	void c2_reg_math(void);
+	void c2_math_1(void);
+	void c2_math_2(void);
+	void c2_math_5(void);
+	void c2_math_51(void);
+	void c2_math_6(void);
+	void c2_math_7(void);
+	void c2_math_8(void);
+	void c2_math_9(void);
+	void c2_math_add(void);
+	void c2_math_sub(void);
+	void c2_math_mult(void);
+	void c2_math_div(void);
+	void c2_math_98(void);
+	void c2_math_99(void);
+
+/* 	       c2z_mem.c		*/
+	void c2_mempy(void);
+	void c2_memmove(void);
+
+
+/* 	       c2z_misc.c		*/
+	void c2_regs(void);
+	void c2_isalpha(void);
+	void c2_isspace(void);
+	void c2_main(void);
+	void c2_test(void);
+	void c2z_ifndef(void);
+	void c2z_endif(void);
+	void c2z_if_pass1(void);
+		
+
+/* 	       c2z_paramaters.c	*/
+	void c2_param_ct(void);
+	void c2_param_ct_2(void);
+	void c2_param_ct_3(void);
+	void c2_param_ct_4(void);
+	void c2_param_ct_5(void);
+	void c2_param_ct_6(void);
+	void c2_param_ct_7(void);
+	void c2_param_ct_8(void);
+	void c2_param_ct_9(void);
+	void c2_param_ct_10(void);
+	void c2_param_ct_11(void);
+
+/* 	       c2z_pass_2.c		*/
+	void scan_func_name_2(void);
+	void scan_func_name_3(void);
+	void c2_ret_2(void);
+	void c2_math_literal(void);
+       void c2_math_literal_1(void);
+	void c2_math_literal_6(void);
+       void c2_math_literal_7(void);
+	void c2_math_literal_8(void);
+	void c2_math_literal_99(void);
+       void c2_pass2_sc1(void);
+	void c2_pass2_sc2t(void);
+	void c2_pass2_if(void);
+	void c2_pass2_if_1(void);
+	void c2_pass2_if_2(void);
+       void c2_pass2_if_4(void);
+       void c2_pass2_if_10(void);
+	void c2_pass2_if_13(void);
+	void c2_pass2_if_15(void);
+	void c2_pass2_whl(void);
+	void c2_pass2_whl_1(void);
+	void c2_pass2_whl_2(void);
+	void c2_pass2_whl_5(void);
+	void c2_pass2_whl_7(void);
+	void c2_pass2_whl_13(void);
+	void c2_pass2_math(void);
+       void c2_pass2_math_1(void);
+	void c2_pass2_math_5(void);
+	void c2_pass2_math_51(void);
+	void c2_pass2_strrev(void);
+       void c2_pass2_fgets(void);
+
+
+/* 	       c2z_print.c		*/
+	void c2_fprintf(void);
+	void c2_printf(void);
+	void c2_printf_literal(void);
+	void c2_printf_numeric(void);
+	void c2_printf_string(void);
+	void c2_printf_dec_only(int);
+	void c2_snprintf(void);
+	void c2_printf_float(void);
+
+
+/*		c2z_realloc.c		*/
+	void c2_scan_realloc(void);
+
+
+/*		c2z_scanf.c		*/
+	void c2_scanf(void);
+	void c2_pass2_sizeof(void);
+
+
+/*		c2z_sizeof.c		*/
+	void c2_sizeof(void);
+
+
+/*		c2z_strcat.c		*/
+	void c2_strcat(void);
+
+
+/*		c2z_strchr.c		*/
+	void c2_strchr(void);
+	void c2_strchr_scan(void);
+
+
+/*		c2z_strcmp.c		*/
+	void c2_strcmp(void);
+	void c2_strcmp_1(void);
+	void c2_strcmp_2(void);
+	void c2_strcmp_3(void);
+
+
+/*		c2z_strcpy.c		*/
+	void c2_strcpy(void);
+	void c2_str_1(int,int);
+	void c2_str_2(int,int);
+	void c2_str_3(int,int);
+	void c2_str_4(void);
+	void c2_str_5(void);
+       void c2_str_6(void);
+ 	void c2_strcpy_pass2(void);
+	void c2_pass2_strcpy_2(int,int);
+
+
+/*		c2z_strlen.c		*/
+	void c2_strlen(void);
+	void c2_strlen_punch(void);
+	void c2_strlen_pass2(void);
+
+/*		c2z_strncpy.c		*/
+	void c2_strncpy(void);
+
+
+/*		c2z_strrchr.c		*/
+	void c2_strrchr(void);
+
+
+/*		c2z_strrev.c		*/
+	void c2_strrev(void);
+
+
+/*		c2z_strset.c		*/
+	void c2_strset(void);
+
+
+/*		c2z_strstr.c		*/
+	void c2_strstr(void);
+	void c2_strstr_scan(void);
+
+/*		c2z_struct.c		*/
+	void c2z_struct(void);
+	void c2z_struct_3(void);
+
+/* 	       c2z_time.c		*/
+	void c2_time(void);
+	void c2_compute_time(void);
+	
+/*            c2z_utility.c         */
+	void write_remark();	
+	void write_remark_long();
+	void write_short(); 
+	void write_variable(); 
+	void check_blank(void);
+	void check_length(void);
+	void check_continuation(void);
+	void check_semi(void);
+	void change_case(void);
+	void verbose(void);
+	void trace_rec_1(void);
+	void trace_rec_2(void);
+	void trace_rec_3(void);
+	void c2_white(void);
+	void src_line(void);
+	void c2_error(void);
+	 int c2_iswhite(int);
+	 int c2_iswhiter(int);
+
+/* 	       c2z_while.c		*/
+	void c2_while(void);
+	void c2_while_1(void);
+	void c2_while_2(void);
+	void c2_while_3(void);
+	void c2_while_4(void);
+	void c2_while_5(void);
+	void c2_while_6(void);
+	void c2_while_7(void);
+	void c2_while_8(void);
+	void c2_while_9(void);
+	void c2_while_10(void);
+	void c2_while_11(void);
+	void c2_while_12(void);
+	void c2_while_13(void);
+	void c2_while_14(void);
+	void c2_while_15(void);
+	void c2_while_16(void);
+			
