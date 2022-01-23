bb06ee58a9c57fc19c44cbc4b68b6ab5846a78e5
root <tchandler48@gmail.com>
2022-01-23 16:18:45 -0600

    Updates

---
+++ examples/ctest5.c
@@ -0,0 +1,121 @@
+ 
+#define MAX_VARS     12
+#define VAR_NAM      16
+#define VAR_NAME     16
+	
+	int t;
+	int x;
+	int y;
+	int z;
+	int I;
+       int imax_vars=0;
+      char varname[VAR_NAM];
+       int ndx;
+       int ivalue;
+      char cvalue[7];
+      char in_stack[MAX_VARS][VAR_NAM];
+       int iv_stack[MAX_VARS];
+      char a[20];
+      char b[20];
+      char p_string[75];
+       int gv_ct;
+
+
+struct variables
+{
+   int gv_rct;
+  char gv_name[VAR_NAME];
+  char gv_cname[VAR_NAME];
+  char gv_type[2];
+   int gv_lgth;
+   int gv_current_lgth;
+  char gv_value[VAR_NAME];
+   int gv_init;
+  char gv_literal[BUFSIZE];
+   int gv_use_ct;
+  char gv_dsect[VAR_NAME];
+   int gv_row;
+   int gv_column;
+  char gv_label[VAR_NAME];
+  char gv_table[VAR_NAME];
+  char gv_aname[VAR_NAME];
+  char gv_sv_reg[VAR_NAME];
+  char gv_wk_reg[VAR_NAME];
+  char gv_wk_strg[VAR_NAME];
+   int gv_flag;
+};
+
+struct variables    *gw_variable;
+	
+int main(int argc, char *argv[])
+	{
+		printf("Hello Z390 World ctest6\n");
+		
+              strcpy(a, "123456");
+              printf("a = %s\n",a);
+
+              strcpy(b, a);
+              printf("b = %s\n",b);
+
+              t = 0;
+              strcpy(in_stack[t], "123456");
+              printf("in_stack[t] = %s\n",in_stack[t]);
+
+              strcpy(varname, "abcdefg");
+              printf("varname = %s\n",varname);
+ 
+              t = 1;
+              strcpy(in_stack[t], varname);
+              printf("in_stack[t] = %s\n",in_stack[t]);
+
+              strcpy(in_stack[1], varname);
+              printf("in_stack[1] = %s\n",in_stack[1]);
+
+              strcpy(p_string, in_stack[1]);
+              printf("p_string #1 = %s\n",p_string);
+
+              t = 1;
+              strcpy(p_string, in_stack[t]);
+              printf("p_string #2 = %s\n",p_string); 
+
+              t = 1;  
+              x = 2;
+              strcpy(in_stack[x], in_stack[t]);
+              printf("in_stack[x] = %s\n",in_stack[x]);
+
+              t = 3;
+              strcpy(in_stack[3], in_stack[x]);
+              printf("in_stack[3] = %s\n",in_stack[t]);
+
+
+              gv_ct = 1;
+              strcpy(gw_variable[gv_ct].gv_name, varname);
+              strcpy(gw_variable[3].gv_name, varname);
+
+              gv_ct = 2;
+              strcpy(gw_variable[gv_ct].gv_name, "123456");
+              strcpy(gw_variable[3].gv_name, "123456");
+
+              x = 4;
+              strcpy(in_stack[x], gw_variable[gv_ct].gv_name);
+
+              gv_ct = 0;
+              strcpy(gw_variable[gv_ct].gv_name, in_stack[x]);
+
+              x = 5;
+		for(I=0; I < x; I++)
+			{
+				printf("in_stack = %s\n", in_stack[I]);
+			}
+		printf(" \n");
+
+
+              x = 5;
+              for(I=0; I < x; I++)
+              {
+                printf("gv_name = %s\n",gw_variable[I].gv_name);
+              }
+
+
+	}
+  
---
+++ examples/ctest6.c
@@ -0,0 +1,106 @@
+ 
+#define MAX_VARS     12
+#define VAR_NAM      16
+#define VAR_NAME     16
+	
+	int t;
+	int x;
+	int y;
+	int z;
+	int I;
+       int imax_vars=0;
+      char varname[VAR_NAM];
+       int ndx;
+       int ivalue;
+      char cvalue[7];
+      char in_stack[MAX_VARS][VAR_NAM];
+       int iv_stack[MAX_VARS];
+      char a[20];
+      char b[20];
+      char c[20];
+      char p_string[75];
+       int gv_ct;
+
+
+struct variables 						
+	{
+		 int gv_rct;
+		char gv_name[VAR_NAME];
+		char gv_cname[VAR_NAME];
+		char gv_type[2];
+		 int gv_lgth;
+		 int gv_current_lgth;
+	       char gv_value[VAR_NAME];
+		 int gv_init;
+		char gv_literal[BUFSIZE];
+		 int gv_use_ct;
+		char gv_dsect[VAR_NAME];
+		 int gv_row;
+		 int gv_column;
+		char gv_label[VAR_NAME];
+	 	char gv_table[VAR_NAME];
+		char gv_aname[VAR_NAME];
+		char gv_sv_reg[VAR_NAME];
+		char gv_wk_reg[VAR_NAME];
+		char gv_wk_strg[VAR_NAME];
+		 int gv_flag;
+	};
+
+  struct variables         *gw_variable;                  
+
+
+
+	
+int main(int argc, char *argv[])
+	{
+		printf("Hello Z390 World ctest7\n");
+		
+              strcpy(a, "123456");
+              printf("a = %s\n",a);
+
+              strcpy(b, "abcdefghi");
+              printf("b = %s\n",b);
+
+              strcat(a,b);
+              printf("a = %s\n",a);
+
+              t = 0;
+              strcpy(in_stack[t], "123456");
+              printf("in_stack[t] = %s\n",in_stack[t]);
+              printf("Here #1\n");
+              strcat(in_stack[t], b);
+
+              printf("Here #2\n");
+              t = 1;
+              strcat(in_stack[t], "zyxwtu");
+              
+              printf("Here #3\n");
+              strcat(in_stack[2], b);
+            
+              printf("Here #4\n"); 
+              strcat(in_stack[3], "yefrtyu");
+             
+
+              x = 5;
+		for(I=0; I < x; I++)
+			{
+				printf("in_stack = %s\n", in_stack[I]);
+			}
+		printf(" \n");
+
+              t = 0;
+              strcpy(gw_variable[t].gv_name, "123456");
+              printf("gw_variable[t].gv_name = %s\n",gw_variable[t].gv_name);
+              printf("Here #5\n");
+              gv_ct = 1;
+              strcat(gw_variable[gv_ct].gv_name, b);
+
+              x = 5;
+              for(I=0; I < x; I++)
+              {
+                printf("gv_name = %s\n",gw_variable[I].gv_name);
+              }
+
+
+	}
+  
